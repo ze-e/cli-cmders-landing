98be93e92cfb6f599e4ce38d8bf5a0a46eca3a10
zrr <zrexrodriguez@gmail.com>
2021-07-15 16:26:57 -0600

    created skeleton

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
 import Facts from './components/Facts/Facts'
+
+import TerryChill from './img/Terrychill.png'
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Intro />
       <Facts />
+      <img className="App__TerryChill" src={TerryChill} />
     </div>
   );
 }
--- src/components/Facts/Facts.js
+++ src/components/Facts/Facts.js
@@ -2,16 +2,19 @@ function Facts(props){
 
   return(
       <section className='facts'>
+        <h2 className="facts__title">Terry Facts</h2>
           <ul className="facts__stats">
-            <li className="facts__stat">Full name: Terrence Shlomo Tegegne </li>
-            <li className="facts__stat">Age: 28 </li>
-            <li className="facts__stat">Occupation: freelance UX designer </li>
-            <li className="facts__stat">Motivation: is learning web development to enter an adjacent field</li>
-            <li className="facts__stat">Hometown: Greater Boston</li>
-            <li className="facts__stat">My Jam: Gardening</li>
-            <li className="facts__stat">My Other Jam: Volunteering at the animal shelter</li>
-            <li className="facts__stat">My Animal Jam: Goats</li>
-            <li className="facts__stat">My Anime Jam: Dragon Ball Z</li>
+            <li className="facts__stat"><b>Full name:</b><br/><br/> Terrence Shlomo Tegegne </li>
+            <li className="facts__stat"><b>Age:</b><br/><br/> 28 </li>
+            <li className="facts__stat"><b>Occupation:</b><br/><br/> freelance UX designer </li>
+            <li className="facts__stat"><b>Motivation:</b><br/><br/> is learning web development to enter an adjacent field</li>
+            <li className="facts__stat"><b>Hometown:</b><br/><br/> Greater Boston</li>
+            <li className="facts__stat"><b>Favorite Color:</b><br/><br/> #0882FF</li>
+            <li className="facts__stat"><b>My Jam:</b><br/><br/> Gardening</li>
+            <li className="facts__stat"><b>My Other Jam:</b><br/><br/> Volunteering at the animal shelter</li>
+            <li className="facts__stat"><b>My Animal Jam:</b><br/><br/> Goats</li>
+            <li className="facts__stat"><b>My Anime Jam:</b><br/><br/> Dragon Ball Z</li>
+            <li className="facts__stat"><b>My Game Jam:</b><br/><br/> Pokemon Go!</li>
           </ul>
       </section>
   )
